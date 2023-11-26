1b5cc130ff77c7663caca1e066c85f92875615ef
Martinb113 <martinb113@gmail.com>
2023-11-26 15:53:46 +0100

    CSS added shop list exc

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import '../styles/styles.css'; // Import the CSS file
 
 const ShoppingList = ({ shoppingLists, onDelete }) => {
   return (
-    <div>
-      <h2>Shopping Lists</h2>
-      <ul>
-        {shoppingLists.map((list) => (
-          <li key={list.id}>
-            {list.name} - {list.description}{' '}
-            <button onClick={() => onDelete(list.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+    <div className="shopping-lists">
+      {shoppingLists.map((list) => (
+        <div key={list.id} className="shopping-list-item">
+          <h3>{list.name}</h3>
+          <p>{list.description}</p>
+          <button onClick={() => onDelete(list.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 };
