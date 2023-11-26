24f9784c053bd05986d64ef8a4c646ec32eb10a5
Martinb113 <martinb113@gmail.com>
2023-11-26 14:26:09 +0100

    Working prottype

--- src/components/AddShoppingListForm.jsx
+++ src/components/AddShoppingListForm.jsx
@@ -15,14 +15,22 @@ const AddShoppingListForm = ({ onAdd }) => {
     <div>
       <h2>Add Shopping List</h2>
       <form onSubmit={handleSubmit}>
-        <label>
+        <label htmlFor="nameInput">
           Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            id="nameInput"  // Add unique id
+            name="name"     // Add name attribute
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
-        <label>
+        <label htmlFor="descriptionInput">
           Description:
           <input
             type="text"
+            id="descriptionInput"  // Add unique id
+            name="description"     // Add name attribute
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
