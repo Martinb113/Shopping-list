51e9bf6188aa25b325c549755aed45583116a3d1
Martinb113 <martinb113@gmail.com>
2023-12-22 23:12:17 +0100

    ListTile implemented

--- src/components/ShoppingListTile.jsx
+++ src/components/ShoppingListTile.jsx
@@ -1,12 +1,27 @@
 // ShoppingListTile.jsx
 import React from 'react';
 
-const ShoppingListTile = ({ shoppingList, onSelect }) => {
+const ShoppingListTile = ({ shoppingList, onSelect, isExpanded }) => {
+  const renderItemDetails = (item) => {
+    return (
+      <div key={item.id} className="item-details">
+        <span>Name: {item.name}</span>
+        <span>Quantity: {item.quantity}</span>
+        <span>Purchased: {item.purchased ? 'Yes' : 'No'}</span>
+        {/* Additional item details or actions can be added here */}
+      </div>
+    );
+  };
+
   return (
-    <div onClick={() => onSelect(shoppingList.id)}>
+    <div className="shopping-list-tile" onClick={() => onSelect(shoppingList.id)}>
       <h3>{shoppingList.name}</h3>
       <p>{shoppingList.description}</p>
-      {/* More details can be displayed as needed */}
+      {isExpanded && (
+        <div className="list-item-container">
+          {shoppingList.items.map(renderItemDetails)}
+        </div>
+      )}
     </div>
   );
 };
